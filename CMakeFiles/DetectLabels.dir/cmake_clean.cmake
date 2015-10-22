2723ef421395ab451266241d000c06bc9ed30f3e
mashikag <littlesoldiergraba@gmail.com>
2015-10-22 02:38:02 +0100

    Corrected errors from the first commit, built, compiled and tested.

---
+++ CMakeFiles/DetectLabels.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/DetectLabels.dir/detectLabels.cpp.o"
+  "DetectLabels.pdb"
+  "DetectLabels"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/DetectLabels.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
